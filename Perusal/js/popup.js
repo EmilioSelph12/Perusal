955839ad33ae3844087a98ca972c763becdb418e
EmilioSelph12 <emilioselph12@gmil.com>
2020-12-03 16:34:16 +0100

    Criação do repositório

---
+++ Perusal/js/popup.js
@@ -0,0 +1,15 @@
+
+function toggle(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
+
+function toggle1(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var cadastrar = document.getElementById('cadastrar');
+    cadastrar.classList.toggle('active');
+}
+
